dd91e4929c944cfcaba0e120b998a9d7b7c0ab68
franckp1 <74624674+franckp1@users.noreply.github.com>
2023-06-15 00:34:16 -0500

    Update __init__.py

--- cosa/prog/__init__.py
+++ cosa/prog/__init__.py
@@ -1,7 +1,7 @@
-import agregar 
-import actu 
-import borrar
-import principal
-import ver_datos
+from cosa.prog import agregar 
+from cosa.prog import actu 
+from cosa.prog import borrar
+from cosa.prog import principal
+from cosa.prog import ver_datos
 
 
