7c5418823a13f0eb6dd32a777c284eba88f04a11
franckp1 <74624674+franckp1@users.noreply.github.com>
2023-06-15 00:36:13 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lols0",
-    version="0.3",
+    version="0.4",
     license="MIT",
     description="base de datos",
     author="Andres Pérez C",
